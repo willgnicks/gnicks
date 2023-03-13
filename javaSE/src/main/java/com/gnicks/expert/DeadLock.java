2d6fe16b6739e1e400ecd04d43704ecacea05d25
willgnicks <gnicksfaith@gmail.com>
2023-03-13 22:40:22 +0800

    fix deep search method

--- javaSE/src/main/java/com/gnicks/expert/DeadLock.java
+++ javaSE/src/main/java/com/gnicks/expert/DeadLock.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
  * @Date 2023/3/11 01:28
  * @Version 1.8
  */
-public class DeadLock {
+public class _Expert_DeadLock {
 
     /**
      * 线程P的需求资源有一个被别人占有，那么就需要等待，不能释放手中的资源
@@ -21,18 +21,20 @@ public class DeadLock {
      */
     public static void main(String[] args) {
 
+
         MyTread[] data = {
                 new MyTread(1, new int[]{40}, new int[]{10}),
                 new MyTread(2, new int[]{10}, new int[]{30}),
-                new MyTread(3, new int[]{30}, new int[]{100,40}),
-                new MyTread(6, new int[0], new int[]{100}),
-                new MyTread(10, new int[]{100}, new int[]{}),
-                new MyTread(14, new int[]{80}, new int[]{70}),
+                new MyTread(3, new int[]{30,90}, new int[]{100,40}),
+                new MyTread(6, new int[]{20}, new int[]{}),
+                new MyTread(10, new int[]{100}, new int[]{80}),
+                new MyTread(14, new int[]{80}, new int[]{70,90}),
                 new MyTread(19, new int[]{70}, new int[]{60}),
                 new MyTread(11, new int[]{60}, new int[]{80}),
+                new MyTread(17, new int[]{50}, new int[]{10}),
         };
-        DeadLock deadLock = new DeadLock();
-        System.out.println(Arrays.stream(deadLock.getDeadLock(Arrays.asList(data))).boxed().collect(Collectors.toList()));
+        _Expert_DeadLock expert_deadLock = new _Expert_DeadLock();
+        System.out.println(Arrays.stream(expert_deadLock.getDeadLock(Arrays.asList(data))).boxed().collect(Collectors.toList()));
     }
 
     public int[] getDeadLock(List<MyTread> threadList) {
@@ -69,29 +71,35 @@ public class DeadLock {
      * @return
      */
     private boolean dfs(Node node, Stack<Integer> stack) {
-        // 修改节点状态 入栈
-        // 压当前节点 并修改状态
+        // 修改当前顶点状态 入栈
         stack.push(node.tid);
         node.isVisited = true;
 
-        // 没有子节点，不具备死锁条件
+        // 该顶点没有依赖，不具备死锁条件 出栈并通知其父节点我并未找到死锁
         Set<Node> dependencies = node.dependencies;
         if (dependencies == null ){
-            stack.pop(); // 弹栈
-            return false; // 告诉上一层我并非死锁一员
+            stack.pop();
+            return false;
         }
 
+        // 标识符 如果子顶点出现死锁，该值将会锁定，锁定该节点为死锁一环
         boolean isDead = false;
         for (Node dependency : dependencies) {
-            // 如果当前节点的子节点出现在栈里，那么就形成回环了
-            // 找到死锁了，如果在栈里出现了该元素，那么该元素必定已经访问过了
-            if (stack.contains(dependency.tid)){
-                isDead = true;
+            // 一旦子顶点中出现死锁，直接进行跳过，判定该顶点为死锁一环
+            if (isDead){
+                return true;
             }
 
-            // 如果没有在，那么继续深入探索
+            // 如果当前顶点的子顶点出现在栈里，那么说明搜索路径上跟该顶点的向形成回环
+            // 找到死锁了，如果在栈里出现了该元素，那么该元素必定已经访问过了，下方的递归调用将会跳过
+            if (stack.contains(dependency.tid)){
+                return true; // 有一个依赖形成回环，即可判定该顶点已在死锁环里了，不需要进行下续判断了
+            }
+
+            // 如果栈里没有该元素，那么继续深入探索
             if (!dependency.isVisited){
-                isDead = dfs(dependency, stack);
+                // 记录递归的状态，如果下方出现死锁环
+                 isDead = dfs(dependency, stack);
             }
         }
         // 在返回给递归调用者时，如果没有找到死锁，那么把当前入栈元素弹出
